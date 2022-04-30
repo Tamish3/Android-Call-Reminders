fa6f673ab4124cdc4ab25c01c8613b9ecabdac18
Selena Xiao <55109903+selenaxiao@users.noreply.github.com>
2022-04-30 17:44:28 -0400

    call receiver temp

--- app/src/main/java/course/labs/todomanager/CallReceiver.kt
+++ app/src/main/java/course/labs/todomanager/CallReceiver.kt
@@ -6,11 +6,16 @@ import android.content.Intent
 import android.telephony.TelephonyManager
 import android.util.Log
 import android.widget.Toast
+import java.util.*
 
 class CallReceiver : BroadcastReceiver() {
     companion object {
         internal const val TAG = "Receiver"
+
     }
+
+    var lastcall = Date()
+    var phonenum = String()
 
     override fun onReceive(context: Context, intent: Intent) {
         Log.i(TAG, "Broadcast Received")
@@ -18,16 +23,18 @@ class CallReceiver : BroadcastReceiver() {
 
         //A contact calls you
         if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-            if (intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER).equals("")) {
-
+            if (intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER).equals(phonenum)) {
+                //lastcall = current time
             }
         }
         //You call a contact
         else if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
-            if (intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER).equals("")) {
-
+            if (intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER).equals(phonenum)) {
+                //lastcall = current time
             }
         }
+
+        //send lastcall to todoitem for countdown calculation
 
     }
 }
