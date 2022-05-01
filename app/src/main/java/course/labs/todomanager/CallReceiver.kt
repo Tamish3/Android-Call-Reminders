781d315253874383bb82be5e070dec2ec185acf7
Selena Xiao <55109903+selenaxiao@users.noreply.github.com>
2022-04-30 22:35:12 -0400

    Get incoming number and name

--- app/src/main/java/course/labs/todomanager/CallReceiver.kt
+++ app/src/main/java/course/labs/todomanager/CallReceiver.kt
@@ -17,65 +17,25 @@ class CallReceiver : BroadcastReceiver() {
         internal const val TAG = "Receiver"
 
     }
-
-    var lastcall = Date()
-    var phonenum = "(650) 555-6789"
+//
+//    var lastcall = Date()
+//    var phonenum = "+16505556789"
 
     //https://www.youtube.com/watch?v=rlzfcqDlovg
+    //https://stackoverflow.com/questions/1853220/retrieve-incoming-calls-phone-number-in-android?noredirect=1&lq=1
     override fun onReceive(context: Context, intent: Intent) {
         Log.i(TAG, "Broadcast Received")
 //        Toast.makeText(context, "Broadcast Received by Receiver", Toast.LENGTH_LONG).show()
 //        Toast.makeText(context, getContactName(phonenum, context), Toast.LENGTH_LONG).show()
 //        Toast.makeText(context, TelephonyManager.EXTRA_INCOMING_NUMBER, Toast.LENGTH_LONG).show()
 
-
         val state = intent.getStringExtra(TelephonyManager.EXTRA_STATE)
         val incomingNumber = intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
-        if (state == TelephonyManager.EXTRA_STATE_RINGING) {
-//            Toast.makeText(context, "Incoming Call State", Toast.LENGTH_SHORT).show()
-//            Toast.makeText(
-//                context,
-//                "Ringing State Number is -$incomingNumber",
-//                Toast.LENGTH_SHORT
-//            ).show()
-        }
-        if (state == TelephonyManager.EXTRA_STATE_OFFHOOK) {
-//            Toast.makeText(context, "Call Received State", Toast.LENGTH_SHORT).show()
-        }
-//        Toast.makeText(
-//                context,
-//                "Ringing State Number is -$incomingNumber",
-//                Toast.LENGTH_SHORT
-//            ).show()
+
         if (incomingNumber != null) {
             Log.i(TAG, "$incomingNumber")
+            Toast.makeText(context, getContactName(incomingNumber, context), Toast.LENGTH_LONG).show()
         }
-
-
-        //name, id
-        //A contact calls you
-//        if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)) {
-//            Toast.makeText(context, intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER), Toast.LENGTH_LONG).show()
-//            if (intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER).equals(phonenum)) {
-//
-//
-//            }
-//            val telephony = context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-//            telephony.listen(object : PhoneStateListener() {
-//                override fun onCallStateChanged(state: Int, incomingNumber: String) {
-//                    super.onCallStateChanged(state, incomingNumber)
-//                    println("incomingNumber : $incomingNumber")
-//                    Toast.makeText(context, "$incomingNumber", Toast.LENGTH_LONG).show()
-//                }
-//            }, PhoneStateListener.LISTEN_CALL_STATE)
-
-//        }
-        //You call a contact
-//        else if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
-//            if (intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER).equals(phonenum)) {
-//
-//            }
-//        }
 
 
     }
